655032f339232afb01fe842fcfbf56816d8d5658
Tim Eckel <tim@leethost.com>
2017-12-10 14:45:48 -0500

    README changes

--- src/NewPing.h
+++ src/NewPing.h
@@ -3,12 +3,12 @@
 //
 // AUTHOR/LICENSE:
 // Created by Tim Eckel - teckel@leethost.com
-// Copyright 2017 License: Forks, distribution, and derivitive works are NOT
-// permitted without permission. Permission is only granted to use as-is for
-// private and commercial use. If you wish to contribute, make changes, or
-// enhancements, please create a pull request as forks and derivitive releases
-// are NOT allowed. I get a TON of support issues from this library, most of
-// which comes from buggy forks or sites without proper documentation.
+// Copyright 2017 License: Forks and derivitive works are NOT permitted without
+// permission. Permission is only granted to use as-is for private and
+// commercial use. If you wish to contribute, make changes, or enhancements,
+// please create a pull request. I get a TON of support issues from this
+// library, most of which comes from old versions, buggy forks or sites without
+// proper documentation, just trying to wrangle this project together.
 //
 // LINKS:
 // Project home: https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home
